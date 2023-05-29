4f88f03d96ad7b130932b3a24f80d49df0ac9877
victor toledo <querynetworking@gmail.com>
2023-05-29 20:12:53 -0300

    creacion de la conexion a base datos mongo

---
+++ index.js
@@ -0,0 +1,8 @@
+// Importar dependencias
+const connection = require("./database/connection");
+
+// Menejsae bienvenida
+console.log("API NODE RED SOCIAL run")
+
+// Conexion a database
+connection();
