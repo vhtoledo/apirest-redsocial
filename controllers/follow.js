79fdc9905f872acef6c809b6faa7f9a4cd2207f7
victor toledo <querynetworking@gmail.com>
2023-06-06 22:28:38 -0300

    creacion de modelo follow

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,3 +1,6 @@
+// Importar Modelo
+const Follow = require("../models/follow");
+const User = require("../models/user");
 
 // Acciones de prueba
 const pruebaFollow = (req, res) => {
@@ -5,6 +8,14 @@ const pruebaFollow = (req, res) => {
         message: "Mensaje enviado desde: controllers/follow.js"
     });
 }
+
+// Acción de guardar un follow (acción seguir)
+
+// Acción de borrar un follow (acción dejar de seguir)
+
+// Acción de listado de usuarios que estoy siguiendo
+
+// Acción de listado de usuarios que me siguen
 
 // Exportar acciones
 module.exports = {
