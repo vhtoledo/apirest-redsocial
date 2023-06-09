5d9ba0da5ee0d9f7aa6f1a947129d3054ec40030
victor toledo <querynetworking@gmail.com>
2023-06-09 20:36:59 -0300

    guardar publicacion

--- controllers/publication.js
+++ controllers/publication.js
@@ -7,7 +7,40 @@ const pruebaPublication = (req, res) => {
     });
 }
 
+// Guardar publicacion
+const save = (req, res) => {
+
+    // Recoger datos del body
+    const params = req.body;
+
+    // Si no me llegan dar respuesta negativa
+    if(!params.text) return res.status(400).send({status: "error", message: "Debes enviar el texto de la publicación"});
+
+    // Crear y rellenar el objeto del modelo
+    let newPublication = new Publication(params);
+    newPublication.user = req.user.id;
+
+    // Guardar objeto en base de datos
+    newPublication.save()
+                  .then((publicationStore) => {
+                    if(!publicationStore) {
+                        return res.status(404).send({
+                            status: "error",
+                            message: "No se ha guardado publicación"
+                        });
+                    }
+
+                    return res.status(200).send({
+                        status: "success",
+                        message: "Publicación guardad",
+                        publicationStore
+                    });
+                  });
+
+}
+
 // Exportar acciones
 module.exports = {
-    pruebaPublication
+    pruebaPublication,
+    save
 }
--- routes/publication.js
+++ routes/publication.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const PublicationController = require("../controllers/publication");
+const check = require("../middlewares/auth");
 
 // Definir rutas
 router.get("/prueba-publication", PublicationController.pruebaPublication);
+router.post("/save", check.auth, PublicationController.save);
 
 // Exportar router
 module.exports = router;
